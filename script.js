727c32a281c4788c071d6618bdf47f79bbfde9c6
Nayan <nayanbirla9893@gmail.com>
2023-12-27 14:11:36 +0530

    episode 1

---
+++ script.js
@@ -0,0 +1,21 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "HEY I AM A HQ TAG")
+  )
+);
+
+console.log(parent);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading1", xyz: "abc" },
+  "Hello World from React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
